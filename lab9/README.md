6e0e502bc28d3e780e65622d409c9cb7f71a2518
Vittorix99 <vittorio.digiorgio@hotmail.it>
2023-12-29 19:25:21 +0100

    Upgrade lab 9

--- lab9/README.md
+++ lab9/README.md
@@ -1,17 +1,18 @@
-# Progetto Algoritmo Genetico - README
 
-## Autore
-Il codice in questo repository è stato sviluppato con l'aiuto di Antonio Ferrigno, matricola S316467, che ha contribuito in alcune sezioni specifiche del progetto.
+# Genetic Algorithm Project - README
 
-## Descrizione del Codice
-Questo progetto implementa un algoritmo genetico per risolvere il problema OneMax. L'obiettivo è massimizzare la funzione di fitness, che è la somma dei valori in un vettore binario, cercando di ottenere tutti i bit impostati a 1. Inoltre, l'algoritmo è progettato per trovare le configurazioni di parametri che minimizzano la chiamata alla funzione di fitness.
+## Author
+The code in this repository was developed with the assistance of Antonio Ferrigno, student number S316467, who contributed to specific sections of the project.
 
-### Parametri dell'Algoritmo
-Durante l'esecuzione dell'algoritmo,viene eseguito l'algoritmo evolutivo facendo variare alcuni parametri:
+## Code Description
+This project implements a genetic algorithm to solve the OneMax problem. The goal is to maximize the fitness function, which is the sum of the values in a binary vector, aiming to have all bits set to 1. Moreover, the algorithm is designed to find parameter configurations that minimize the calls to the fitness function.
 
-- **Population Size**: La dimensione della popolazione di individui considerati durante ogni generazione.
+In the latest version, a memoization technique has been introduced to store the fitness of individuals that have already been calculated. This enhancement significantly improves the efficiency of the algorithm by avoiding redundant fitness calculations for the same individual.
 
-- **Tournament Size**: Il numero di individui selezionati casualmente dalla popolazione per partecipare al torneo di selezione.
+### Algorithm Parameters
+During the execution of the algorithm, the evolutionary algorithm is run with varying parameters:
 
-- **Convergence Rate**: La percentuale di convergenza che indica quando l'algoritmo deve fermarsi, considerando la soluzione raggiunta accettabile.
-
+- **Population Size**: The size of the population of individuals considered during each generation.
+- **Tournament Size**: The number of individuals randomly selected from the population to participate in the tournament selection.
+- **Convergence Rate**: The convergence percentage that indicates when the algorithm should stop, considering the solution reached as acceptable.
+- **Generation**: The number of generations the algorithm should run before terminating. A generation represents a complete cycle of selection, crossover, and mutation of individuals in the population.
--- lab9/lab9.py
+++ lab9/lab9.py
@@ -3,6 +3,10 @@ import numpy as np
 import itertools
 import lab2_lib as lib
 import matplotlib.pyplot as plt
+import pickle
+from tqdm import tqdm
+
+
 
 
 
@@ -17,7 +21,7 @@ def initialize_population(population_size):
 
 
 
-def elitism(population, fitnesses, elite_size=2):
+def elitism(population, fitnesses, elite_size=6):
     sorted_population = sorted(zip(population, fitnesses), key=lambda x: x[1], reverse=True)
     return [individual for individual, _ in sorted_population[:elite_size]]
 
@@ -66,41 +70,86 @@ def tournament_selection(population, fitnesses, selection_size=2):
         
     return selected
 
+def roulette_wheel_selection(population, fitnesses):
+    total_fitness = sum(fitnesses)
+    selection_probabilities = [f/total_fitness for f in fitnesses]
 
-def create_new_population(population, fitnesses, crossover_rate=0.8, mutation_rate=0.01, tournament_size=2):
+    # Cumulative probabilities
+    cumulative_probabilities = np.cumsum(selection_probabilities)
+    
+    # Ruota della fortuna
+    selected = []
+    for _ in range(2):
+        r = np.random.rand()  # Genera un numero casuale tra 0 e 1
+        for i, cp in enumerate(cumulative_probabilities):
+            if r <= cp:
+                selected.append(population[i])
+                break
+
+    return selected
+
+
+def create_new_population(population, fitnesses, crossover_rate=0.8, mutation_rate=0.01, tournament_size=2, sel_type="tournament"):
     new_population = elitism(population, fitnesses)
     while len(new_population) < len(population):
-        parent1, parent2 = tournament_selection(population, fitnesses)
+        parent1, parent2 =  selection_function[sel_type](population, fitnesses, tournament_size) if sel_type == "tournament" else selection_function[sel_type](population, fitnesses)
         child= crossover(parent1, parent2, crossover_rate)
         
         child= mutate(child, mutation_rate)
         new_population.append(child)
     return new_population
 
+def calculate_fitnesses(population, problem):
+  fitnesses = []
+  for individual in population:
+        # Converti l'individuo in una chiave univoca
+        key = pickle.dumps(individual)
+        
+        # Se la fitness di questo individuo è già stata calcolata, usala
+        if key in memoization:
+            fitness = memoization[key]
+        else:
+            # Altrimenti, calcola la fitness e memorizzala nel dizionario
+            fitness = problem(individual)
+            memoization[key] = fitness
+            
+        fitnesses.append(fitness)
+        
+  return fitnesses
+
+
+
+
+
 
 if __name__ == "__main__":
     instances = [1, 2, 5, 10]
     GENOME_SIZE = 1000
-    GENERATIONS = 100
-    tournament_sizes = [3, 5, 7]
+    generations = [150, 250, 350]
+    tournament_sizes = [2, 3, 5, 7]
     crossover_rates = [0.6, 0.8, 1]
-    mutation_rate = 0.1
+    mutation_rate = 0.15
     population_sizes = [20, 50, 100]
-    convrgence_generations = 20
+    convrgence_generations = 30
     problem_istances = [lib.make_problem(n) for n in instances]
     highest_fitnesses_per_params_combination = {}
     best_configuration_generations = {}
 
     best_stats_per_instance = {}
     best_generations_per_instance = {}
+    memoization = {}
+    selection_type = ["tournament", "roulette_wheel"]
+    selection_function = {"tournament": tournament_selection, "roulette_wheel": roulette_wheel_selection}
+    
 
 
+  
 
     for instance, problem in zip(instances, problem_istances):
         highest_fitnesses_per_params_combination = {}   
         best_configuration_generations = {}
 
-        for tournament_size, crossover_rate, population_size in itertools.product(tournament_sizes, crossover_rates, population_sizes):
+        for gen, tournament_size, crossover_rate, population_size, sel_type in itertools.product(generations, tournament_sizes, crossover_rates, population_sizes , selection_type):
                     #inizialiiamo la popolazione
                     population = initialize_population(population_size)
                     best_fitness_of_generation = []
@@ -109,20 +158,19 @@ if __name__ == "__main__":
                     generation_without_improvement = 0
                     problem._calls = 0
                     #ciclo di generazioni (dato un tournament size, crossover rate e population size)
-                    for generation in range(GENERATIONS):
+                    print("==========================================")
+                    print(f"GA with tournament size: {tournament_size}, crossover rate: {crossover_rate}, population size: {population_size}, generation: {gen}, selection type : {sel_type}")
+                    for generation in tqdm(range(gen), desc= "Generation", position=0, leave=True):
 
                         #calcoliamo il fitness di ogni individuo
-                        fitnesses = [problem(individual) for individual in population]
+                        fitnesses = calculate_fitnesses(population, problem)
                         index_max = max(enumerate(fitnesses), key=lambda x: x[1])[0]
                         #calcoliamo il best fitness e il best individual della generazione  
                         individual_max = population[index_max]
                         fitness_max = fitnesses[index_max]
                         best_fitness_of_generation.append((fitness_max, individual_max))
                     
-                        print("==========================================")
-                        print(f"instance: {instance}, tournament_size: {tournament_size}, crossover_rate: {crossover_rate}, population_size: {population_size}, generation: {generation}")
-                        print(f"generation: {generation}, best_fitness: {fitness_max}")
-                        print("==========================================")
+                       
                         #aggiorniamo il best overall
                         if fitness_max >= best_overall[0]:
                             best_overall = (fitness_max, individual_max)
@@ -134,21 +182,26 @@ if __name__ == "__main__":
 
                         #se il contatore raggiunge il numero di generazioni di convergenza, usciamo dal ciclo
                         if generation_without_improvement > convrgence_generations:
+
                             print("End of evolution due to no improvement")
+                            print("Max fitness: ", best_overall[0])
+
+
+
 
                             break
 
                         
                         #selection and replacement
                         #eseguiamo la selezione e la sostituzione (tournament selection, crossover, mutation)
-                        population = create_new_population(population, fitnesses, crossover_rate, mutation_rate, tournament_size)
+                        population = create_new_population(population, fitnesses, crossover_rate, mutation_rate, tournament_size, sel_type)
                     
                     #salviamo il best fitness e il best individual e il numero di fitness_calls per ogni combinazione di tournament size, crossover rate e population size
-                    key =  ( tournament_size, crossover_rate, population_size )
+                    key =  ( tournament_size, crossover_rate, population_size, gen, sel_type)
 
 
                     fitness_calls = problem._calls
-                    value = (best_overall[0], best_overall[1], problem.calls)
+                    value = (best_overall[0], best_overall[1], fitness_calls) #salva il best fitness, il best individual e il numero di fitness calls per ogni combinazione di tournament size, crossover rate e population size e numero di generazioni
 
                     highest_fitnesses_per_params_combination[key] = value
                     #salviamo per ogni configuarazione lo storico dei best fitness 
@@ -156,7 +209,7 @@ if __name__ == "__main__":
 
 
 
-                    print(f"instance: {instance}, tournament_size: {tournament_size}, crossover_rate: {crossover_rate}, population_size: {population_size}, best_fitness: {best_overall[0]}, fitness_calls: {fitness_calls}")
+                    print(f"instance: {instance}, tournament_size: {tournament_size}, crossover_rate: {crossover_rate}, population_size: {population_size}, best_fitness: {best_overall[0]}, fitness_calls: {fitness_calls}, selection type: {sel_type}")
         # Trova la chiave con il massimo value[0] ovvero il best fitness
         key_max = max(highest_fitnesses_per_params_combination.keys(), key=lambda x: highest_fitnesses_per_params_combination[x][0])
 
@@ -174,8 +227,17 @@ if __name__ == "__main__":
     print(best_stats_per_instance)
     print(best_generations_per_instance)
 
+
+    output_file = open("output.txt", "w")
+    output_file.write("==========================================\n")
+    output_file.write("Best statistics per instance:\n")
+    output_file.write(f" Problem Instance 1: Best Configuration: {best_stats_per_instance[1][0][0]} (Fitness value: {best_stats_per_instance[1][0][1]})\n")
+    output_file.write("==========================================\n")
+    output_file.write("==========================================\n")
+    #output_file.write("Best generations per instance:\n")
+    #output_file.write(f" {best_generations_per_instance}")
+    output_file.write("==========================================\n")    
     for problem_instance_number, problem in zip(instances, problem_istances):
-        
         best_stats  = best_stats_per_instance[problem_instance_number]
         best_generations = best_generations_per_instance[problem_instance_number]
         best_configuration = best_stats[0][0]
@@ -186,13 +248,16 @@ if __name__ == "__main__":
             plt.figure()
             plt.plot(range(len(best_generations)), best_generations, marker='o')
             title = f"Best Configuration for Problem Instance {problem_instance_number}: Population Size={best_configuration[2]}, Crossover Rate={best_configuration[1]}, Tournament Size={best_configuration[0]}, Max Generations=100 (Fitness value: {highest_fitness}, Fitness Calls: {fitness_calls})"
-            plt.title(title)
+            plt.title(title, fontsize=10)
             plt.xlabel("Generations")
             plt.ylabel("Best Fitness")
             plt.grid(True)
-            plt.show()
+            plt.savefig(f"best_fitness_problem_number_{problem_instance_number}.png")
+            plt.close()
         
-        print(f"the least number of fitness calls for problem instance {problem_instance_number} is {min_fitness_calls} with the following configuration: {best_stats[1][0]}")   
+        output_file.write(f"the least number of fitness calls for problem instance {problem_instance_number} is {min_fitness_calls} with the following configuration: {best_stats[1][0]}\n")
+
+    output_file.close()
 
 
 
